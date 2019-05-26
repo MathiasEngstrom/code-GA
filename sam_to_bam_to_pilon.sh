0e3307873f8b32f2ef5146df5e0323bc60f5bd7f
MathiasEngstrom <47558302+MathiasEngstrom@users.noreply.github.com>
2019-05-26 13:19:00 +0200

    Update sam_to_bam_to_pilon.sh

--- sam_to_bam_to_pilon.sh
+++ sam_to_bam_to_pilon.sh
@@ -10,4 +10,8 @@
 module load bioinfo-tools
 module load samtools
 # Your commands
-samtools 
+samtools view -S -b BWA-mem_scaffold-11_short_read_alignment_2019-04-05.sam > BWA-mem_scaffold-11_short_read_alignment_2019-04-05.bam
+
+samtools sort BWA-mem_scaffold-11_short_read_alignment_2019-04-05.bam -o BWA-mem_scaffold-11_short_read_alignment_2019-04-05_sorted.bam
+
+pilon --genome /home/maen5548/genome-analysis-2019/data/derived-data/scaffold-11_canu_assembly_2019-04-04/scaffold-11.contigs.fasta --bam /home/maen5548/genome-analysis-2019/data/derived-data/BWA/BWA-mem_scaffold-11_short_read_alignment_2019-04-05_sorted.bam --diploid --threads 4 --output /home/maen5548/genome-analysis-2019/data/derived-data/pilon_corrected_assembly/scaffold-11_pilon_corrected_2019-04-11.fasta
